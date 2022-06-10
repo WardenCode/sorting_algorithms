43cb28b4140497d03f02555a451a9370f373f86e
WardenCode <diegojeanluck@hotmail.com>
2022-06-10 00:02:49 -0500

    Fix: betty style (unexpected N°lines 24 instead 25)

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,36 +1,27 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - 
- *
- * @list: 
+ * insertion_sort_list - Sort a doubly linked list with insertion sort.
+ * @list: Double pointer to an doubly linked list (only numbers).
  *
  * Return: Void.
  */
 
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *head = NULL, *node = NULL, *holder = NULL;
-	listint_t *tmp = NULL;
+	listint_t *node = NULL, *holder = NULL, *tmp = NULL;
 
-	if (!list || !(*list))
+	if (!list || !(*list) || (*list)->next == NULL)
 		return;
 
-	head = *list;
-	if (head->next == NULL)
-		return;
-
-	for (node = head; node; node = node->next)
-	{
+	for (node = *list; node; node = node->next)
 		if (node->next && (node->n > node->next->n))
 		{
 			holder = node->next;
+
 			for (tmp = holder; tmp->prev; tmp = tmp->prev)
 				if (tmp->prev->n < tmp->n)
-				{
-					node = *list;
 					break;
-				}
 
 			if (holder->next && holder->prev)
 			{
@@ -46,8 +37,7 @@ void insertion_sort_list(listint_t **list)
 
 			if (tmp->prev)
 			{
-				tmp->prev->next = holder;
-				holder->prev = tmp->prev;
+				tmp->prev->next = holder, holder->prev = tmp->prev;
 				tmp->prev = holder;
 			}
 			else
@@ -56,8 +46,7 @@ void insertion_sort_list(listint_t **list)
 				holder->prev = NULL;
 				*list = holder;
 			}
-			print_list(*list);
-			node = *list;
+
+			print_list(*list), node = *list;
 		}
-	}
 }
